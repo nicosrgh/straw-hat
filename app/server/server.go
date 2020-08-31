c1e75533d559382895b821479a66ccba1a6cd3f8
nicooozz <nico.saragih@pohondana.id>
2020-08-31 23:29:57 +0700

    [MODIFY] remove unused code

--- app/server/server.go
+++ app/server/server.go
@@ -19,34 +19,30 @@ func Init() {
 		// run the schedule
 
 		// dimension employee
-		// transformer.TitleDimension()
-		// transformer.GenderDimension()
-		// transformer.LocationDimension()
-		// transformer.DepartmentDimension()
-		// transformer.StatusDimension()
+		transformer.TitleDimension()
+		transformer.GenderDimension()
+		transformer.LocationDimension()
+		transformer.DepartmentDimension()
+		transformer.StatusDimension()
 
 		// dimension transaction
-		// transformer.ProductDimension()
-		// transformer.PartnerDimension()
-		// transformer.ClientDimension()
+		transformer.ProductDimension()
+		transformer.PartnerDimension()
+		transformer.ClientDimension()
 
 		// source
-		// transformer.EmployeeSource()
-		// transformer.TransactionSource()
+		transformer.EmployeeSource()
+		transformer.TransactionSource()
 
 		// fact employee
-		// transformer.FactEmployeeGender()
-		// transformer.FactEmployeeLocation()
-		// transformer.FactEmployeeStatus()
+		transformer.FactEmployeeGender()
+		transformer.FactEmployeeLocation()
+		transformer.FactEmployeeStatus()
 
 		// fact transaction
-		// transformer.FactTransactionClient()
-		// transformer.FactTransactionProduct()
+		transformer.FactTransactionClient()
+		transformer.FactTransactionProduct()
 		transformer.FactTransactionPartner()
-
-		// datamart
-		// transformer.EmployeeTitleDatamart()
-		// transformer.EmployeeTotalDatamart()
 
 	})
 
